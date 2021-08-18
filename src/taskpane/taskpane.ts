b09d30285694d7338174ce353cad1ea9f72ca844
Matthew Calvert <mattc@themailshark.com>
2021-08-18 11:09:11 -0400

    umm

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -10,58 +10,58 @@ import "../../assets/icon-80.png";
 
 /* global console, document, Excel, Office */
 
-Office.addin.setStartupBehavior(Office.StartupBehavior.load);
-
-Office.onReady((info) => {
-  if (info.host === Office.HostType.Excel) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    
-    Excel.run(async context => {
-    let sheet = context.workbook.worksheets.getActiveWorksheet();
-    sheet.onChanged.add(onChange);
-
-    await context.sync();
-    console.log("A handler has been registered for the onChanged event.");
-    });
-  };
-
-/**
- * Handle the changed event from the worksheet.
- *
- * @param event The event information from Excel
- */
-  async function onChange(event) {
-    return Excel.run(function(context) {
-      return context.sync().then(function() {
-        console.log("Change type of event: " + event.changeType);
-        console.log("Address of event: " + event.address);
-        console.log("Source of event: " + event.source);
-      });
-    });
-  }
-});
+// Office.addin.setStartupBehavior(Office.StartupBehavior.load);
 
 // Office.onReady((info) => {
 //   if (info.host === Office.HostType.Excel) {
 //     document.getElementById("sideload-msg").style.display = "none";
 //     document.getElementById("app-body").style.display = "flex";
-//     // document.getElementById("run").onclick = run;
+    
+//     Excel.run(async context => {
+//     let sheet = context.workbook.worksheets.getActiveWorksheet();
+//     sheet.onChanged.add(onChange);
+
+//     await context.sync();
+//     console.log("A handler has been registered for the onChanged event.");
+//     });
+//   };
+
+// /**
+//  * Handle the changed event from the worksheet.
+//  *
+//  * @param event The event information from Excel
+//  */
+//   async function onChange(event) {
+//     return Excel.run(function(context) {
+//       return context.sync().then(function() {
+//         console.log("Change type of event: " + event.changeType);
+//         console.log("Address of event: " + event.address);
+//         console.log("Source of event: " + event.source);
+//       });
+//     });
 //   }
 // });
 
-// export async function run() {
-//   try {
-//     await Excel.run(async (context) => {
-//       const range = context.workbook.getSelectedRange();
-//       range.format.fill.color = "yellow";
-//       range.load("address");
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    // document.getElementById("run").onclick = run;
+  }
+});
 
-//       await context.sync();
+export async function run() {
+  try {
+    await Excel.run(async (context) => {
+      const range = context.workbook.getSelectedRange();
+      range.format.fill.color = "yellow";
+      range.load("address");
 
-//       console.log(`The range address was "${range.address}".`);
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
+      await context.sync();
+
+      console.log(`The range address was "${range.address}".`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
