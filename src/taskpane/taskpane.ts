7d3539e72f8a01e48d9019fbbaade3f2772b7b85
Matthew Calvert <mattc@themailshark.com>
2021-08-18 09:33:46 -0400

    add code that will run when document is opened

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -10,26 +10,52 @@ import "../../assets/icon-80.png";
 
 /* global console, document, Excel, Office */
 
-Office.onReady((info) => {
-  if (info.host === Office.HostType.Excel) {
-    document.getElementById("sideload-msg").style.display = "none";
-    document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
-  }
-});
+// Office.onReady((info) => {
+//   if (info.host === Office.HostType.Excel) {
+//     document.getElementById("sideload-msg").style.display = "none";
+//     document.getElementById("app-body").style.display = "flex";
+//     document.getElementById("run").onclick = run;
+//   }
+// });
 
-export async function run() {
-  try {
-    await Excel.run(async (context) => {
-      const range = context.workbook.getSelectedRange();
-      range.format.fill.color = "yellow";
-      range.load("address");
+// export async function run() {
+//   try {
+//     await Excel.run(async (context) => {
+//       const range = context.workbook.getSelectedRange();
+//       range.format.fill.color = "yellow";
+//       range.load("address");
 
-      await context.sync();
+//       await context.sync();
 
-      console.log(`The range address was "${range.address}".`);
+//       console.log(`The range address was "${range.address}".`);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+
+Office.initialize = () => {
+  Excel.run(async context => {
+    let sheet = context.workbook.worksheets.getActiveWorksheet();
+    sheet.onChanged.add(onChange);
+
+    await context.sync();
+    console.log("A handler has been registered for the onChanged event.");
+  });
+};
+
+/**
+ * Handle the changed event from the worksheet.
+ *
+ * @param event The event information from Excel
+ */
+async function onChange(event) {
+  return Excel.run(function(context) {
+    return context.sync().then(function() {
+      console.log("Change type of event: " + event.changeType);
+      console.log("Address of event: " + event.address);
+      console.log("Source of event: " + event.source);
     });
-  } catch (error) {
-    console.error(error);
-  }
+  });
 }
